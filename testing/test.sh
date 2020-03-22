f7b0e3e3dcf7bd46626030737fd8e79709de8042
Hendrik Maus <aidentailor@gmail.com>
2020-03-22 14:34:02 +0100

    Download wait-for-it within test.sh

--- testing/test.sh
+++ testing/test.sh
@@ -2,6 +2,11 @@
 set -eu -o pipefail
 set -x
 
+if [ ! -f "wait-for-it" ]; then
+  wget -O wait-for-it https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
+  chmod +x wait-for-it
+fi
+
 # step 0 - run go build + fix permissions
 docker run --rm \
   -v "$(pwd)/../":/go/src/github.com/hendrikmaus/openhab-auth-router \
@@ -21,13 +26,13 @@ docker-compose up -d
 trap "docker-compose down" EXIT
 
 # wait for openhab itself to listen on port 8080
-wait-for-it localhost:8080 -- echo "openHAB container is up"
+./wait-for-it localhost:8080 -- echo "openHAB container is up"
 
 # wait for nginx to listen on port 80
-wait-for-it localhost:80 -- echo "nginx container is up"
+./wait-for-it localhost:80 -- echo "nginx container is up"
 
 # wait for openhab-auth-router to listen on port 9090
-wait-for-it localhost:9090 -- echo "openhab-auth-router container is up"
+./wait-for-it localhost:9090 -- echo "openhab-auth-router container is up"
 sleep 30
 curl -sL "http://localhost:8080/" | grep "Initial Setup"
 
@@ -57,4 +62,4 @@ curl -sL -u demo:demo "http://localhost/basicui/app?sitemap=demo" | grep "Demo"
 curl -sL -u demo:demo "http://localhost/basicui/app?sitemap=widgetoverview" | grep "Widget Overview"
 
 # cleanup
-rm openhab-auth-router
+rm openhab-auth-router wait-for-it
